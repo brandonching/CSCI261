1f7bbf52db1b8adaebb00f17d37474d3401015c8
Brandon Ching <68457375+brandonching@users.noreply.github.com>
2022-01-19 11:56:31 -0700

    Complete L1A

--- Set1/L1A/main.cpp
+++ Set1/L1A/main.cpp
@@ -1,12 +1,14 @@
-/* CSCI 261 Lab 1A: XXXX (_GIVE_BRIEF_DESCRIPTION_HERE_)
+/* CSCI 261 Set 1: Lab 1A - Math Equation Solver
  *
  * Author: Brandon Ching
- *
- * More complete description here...
+ * Resources: w3school for pow() function
+ * 
+ * Program has two "caculators", one which calculates the current in Ohms law and the other calculates gravitational force  
  */
 
 // The include section adds extra definitions from the C++ standard library.
 #include <iostream> // For cin, cout, etc.
+#include <math.h>   // For pow
 
 // We will (most of the time) use the standard library namespace in our programs.
 using namespace std;
@@ -19,8 +21,6 @@ int main()
 
     /******** INSERT YOUR CODE BELOW HERE ********/
 
-    cout << "Hello world!" << endl; // print Hello world! to the screen
-
     double voltage;
     double resistance;
     cout << "Calculating Ohm's Law --- I = V/R" << endl;
@@ -31,17 +31,18 @@ int main()
     double current = voltage / resistance;
     cout << "The Current is: " << current << "A" << endl;
 
-    const double gravitationalConstant = 6.6743e-11;
-    int mass1;
-    int mass2;
-    int radius;
+    cout << "Calculating Newton's Law of Universal Gravitational --- F = Gm1m2/r^2" << endl;
+    const double gravitationalConstant = 6.6726e-11;
+    double mass1;
+    double mass2;
+    double radius;
     cout << "Enter the Mass of Mass1: ";
     cin >> mass1;
     cout << "Enter the Mass of Mass2: ";
     cin >> mass2;
     cout << "Enter the Radius: ";
     cin >> radius;
-    double force = gravitationalConstant * mass1 * mass2 / (radius ^ 2);
+    double force = (gravitationalConstant * mass1 * mass2) / pow(radius, 2);
     cout << "The Force is: " << force << endl;
 
     /******** INSERT YOUR CODE ABOVE HERE ********/
