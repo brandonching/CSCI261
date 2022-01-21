b63490a1ccf0ede9b961cbadfa65b6ddaeedc7fe
Brandon Ching <68457375+brandonching@users.noreply.github.com>
2022-01-21 12:18:30 -0700

    Template

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* CSCI 261: Assignment 1: A1 - TBA
+ *
+ * Author: Brandon Ching
+ * Resources: N/A
+ *
+ * Description: 
+ */
+
+// Libraries.
+#include <iostream> // For cin, cout, etc.
+
+// Library namespace
+using namespace std;
+
+// Constants
+
+int main()
+{
+
+    return 0;
+}
