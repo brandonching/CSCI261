4a65f30199748df6daddbe52996706e663f96ae2
Brandon Ching <68457375+brandonching@users.noreply.github.com>
2022-01-30 23:57:21 -0700

    Finish L1D

--- Set1/L1D/main.cpp
+++ Set1/L1D/main.cpp
@@ -1,21 +1,42 @@
-/* CSCI 261: Assignment 1: L1D - Find the Min/Max
+/* CSCI 261: Assignment 1: L1D - Holy Loops Batman!
  *
  * Author: Brandon Ching
  * Resources: N/A
  *
- * Description: 
+ * Description: Execute the same repetitive task using 3 diffrent methods:
+ * while, do-while, and for loop
  */
 
 // Libraries.
-#include <iostream> // For cin, cout, etc.
+#include <iostream>  // For cin, cout, etc.
 
 // Library namespace
 using namespace std;
 
 // Constants
 
-int main()
-{
+int main() {
+  // While
+  int count = 1;
+  while (count <= 11) {
+    cout << "Na ";
+    count += 1;
+  }
+  cout << "Batman!" << endl;
 
-    return 0;
+  // Do-While
+  count = 1;
+  do {
+    cout << "Na ";
+    count += 1;
+  } while (count <= 11);
+  cout << "Batman!" << endl;
+
+  // For
+  for (int i = 1; i <= 11; i++) {
+    cout << "Na ";
+  }
+  cout << "Batman!" << endl;
+
+  return 0;
 }
