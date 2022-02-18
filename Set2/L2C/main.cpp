85f60763d88f70fe4a037f2e447dddec6b357d96
Brandon Ching <bching9@gmail.com>
2022-02-18 02:18:03 -0700

    Complete L2C

--- Set2/L2C/main.cpp
+++ Set2/L2C/main.cpp
@@ -3,10 +3,13 @@
  * Author: Brandon Ching
  * Resources: N/A
  *
- * Description:
+ * Description: Use pass by refrence to convert between polar and cartesian
+ * coordiantes
  */
 
 // Libraries
+#include <math.h>
+
 #include <iostream>  // For cin, cout, etc.
 
 // Library namespace
@@ -14,4 +17,45 @@ using namespace std;
 
 // Constants
 
-int main() { return 0; }
+void polar_to_cartesian(const double radius, const double angle, double& xCoord,
+                        double& yCoord) {
+  xCoord = radius * cos(angle);
+  yCoord = radius * sin(angle);
+}
+
+void cartesian_to_polar(const double xCoord, const double yCoord,
+                        double& radius, double& angle) {
+  radius = sqrt(pow(xCoord, 2) + pow(yCoord, 2));
+  angle = atan(yCoord / xCoord);
+}
+
+int main() {
+  bool mode;
+  double radius, angle, xCoord, yCoord;
+  cout << "Wlecome to the Polar-Cartesian Conversion Calculator" << endl
+       << "[0] - Polar to Cartesian - (r, θ) -> (x, y)" << endl
+       << "[1] - Cartesian to Polar - (x, y) -> (r, θ)" << endl
+       << "Please Select a Conversion Mode [0/1] ";
+  cin >> mode;
+  cout << endl;
+
+  // Based on user input convert coordinates correctly
+  if (mode) {
+    cout << "Conveting Cartesian to Polar - (x, y) -> (r, θ)" << endl
+         << "X-Cord: ";
+    cin >> xCoord;
+    cout << "Y-Cord: ";
+    cin >> yCoord;
+    cartesian_to_polar(xCoord, yCoord, radius, angle);
+    cout << "Radius: " << radius << endl << "Angle(radians): " << angle << endl;
+  } else {
+    cout << "Converting Polar to Cartesian - (r, θ) -> (x, y)" << endl
+         << "Radius: ";
+    cin >> radius;
+    cout << "Angle(radians): ";
+    cin >> angle;
+    polar_to_cartesian(radius, angle, xCoord, yCoord);
+    cout << "X-Cord: " << xCoord << endl << "Y-Cord: " << yCoord << endl;
+  }
+  return 0;
+}
