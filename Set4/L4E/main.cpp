4e3cf062494fa57bb5f05c7632d586cd2856a23f
Brandon Ching <bching9@gmail.com>
2022-03-29 10:42:14 -0600

    L4E work in progress

--- Set4/L4E/main.cpp
+++ Set4/L4E/main.cpp
@@ -10,9 +10,34 @@
 // Libraries
 #include <iostream>  // For cin, cout, etc.
 
+#include "LinkedList.cpp"
+
 // Library namespace
 using namespace std;
 
 // Constants
 
-int main() { return 0; }
+int main() {
+  LinkedList list;
+  cout << "List 1 Size: " << list.size() << endl;
+  list.pushFront(1);
+  list.pushBack(2);
+  list.pushFront(3);
+  list.pushBack(4);
+  list.pushFront(5);
+  list.pushBack(6);
+  cout << "List 1 Size: " << list.size() << endl;
+
+  cout << "The Contents of the list are: ";
+  for (unsigned int i = 0; i < list.size(); i++) {
+    cout << list.at(i) << " ";
+  }
+
+  LinkedList list2;
+  cout << endl << "List 2 Size: " << list2.size() << endl;
+  list2 = list;
+  cout << "Set list2 = list1" << endl;
+  cout << "List 1 Size: " << list.size() << endl;
+  cout << "List 2 Size: " << list2.size() << endl;
+  return 0;
+}
