421ec394943c23d8ea5d02d6859d2001c5067c35
Brandon Ching <bching9@gmail.com>
2022-03-28 18:22:24 -0600

    Complete L4B

--- Set4/L4B/linked_list_functions.cpp
+++ Set4/L4B/linked_list_functions.cpp
@@ -1,46 +1,111 @@
 #include "linked_list_functions.h"
 
 Node* linked_list_make_node(const int VALUE) {
-    // TODO #1
-    return nullptr;
+  Node* tempNode = new Node;
+  tempNode->pNext = nullptr;
+  tempNode->value = VALUE;
+  return tempNode;
 }
 
 int linked_list_get_size(Node* pHead) {
-    // TODO #2
-    return -999;
+  int size = 0;
+  Node* currentNode = pHead;
+  while (currentNode != nullptr) {
+    size++;
+    currentNode = currentNode->pNext;
+  }
+  return size;
 }
 
 Node* linked_list_add_value_to_front(Node* pHead, const int VALUE) {
-    // TODO #3
-    return nullptr;
+  Node* tempNode = new Node;
+  tempNode->pNext = pHead;
+  tempNode->value = VALUE;
+  return tempNode;
 }
 
 int linked_list_get_value_at_position(Node* pHead, const int POS) {
-    // TODO #4
-    return -999;
+  if (pHead == nullptr) {
+    return -1;
+  }
+  Node* currentNode = pHead;
+  if (POS < 0) {
+    return -1;
+  }
+  for (int i = 1; i <= POS; i++) {
+    currentNode = currentNode->pNext;
+    if (currentNode == nullptr) {
+      return -1;
+    }
+  }
+  return currentNode->value;
 }
 
 int linked_list_min(Node* pHead) {
-    // TODO #5
-    return -999;
+  if (pHead == nullptr) {
+    return -1;
+  }
+  int minValue = pHead->value;
+  Node* currentNode = pHead;
+  while (currentNode != nullptr) {
+    if (currentNode->value < minValue) {
+      minValue = currentNode->value;
+    }
+    currentNode = currentNode->pNext;
+  }
+  return minValue;
 }
 
 int linked_list_max(Node* pHead) {
-    // TODO #6
-    return -999;
+  if (pHead == nullptr) {
+    return -1;
+  }
+  int maxValue = pHead->value;
+  Node* currentNode = pHead;
+  while (currentNode != nullptr) {
+    if (currentNode->value > maxValue) {
+      maxValue = currentNode->value;
+    }
+    currentNode = currentNode->pNext;
+  }
+  return maxValue;
 }
 
 int linked_list_find(Node* pHead, const int TARGET) {
-    // TODO #7
-    return -999;
+  int index = 0;
+  Node* currentNode = pHead;
+  while (currentNode != nullptr) {
+    if (currentNode->value == TARGET) {
+      return index;
+    }
+    currentNode = currentNode->pNext;
+    index++;
+  }
+  return -1;
 }
 
 Node* linked_list_remove_node_from_front(Node* pHead) {
-    // TODO #8
+  if (pHead == nullptr) {
     return nullptr;
+  }
+  Node* temp = pHead;
+  pHead = temp->pNext;
+  delete temp;
+  return pHead;
 }
 
-Node* linked_list_set_value_at_position(Node* pHead, const int POS, const int VALUE) {
-    // TODO #9
-    return nullptr;
+Node* linked_list_set_value_at_position(Node* pHead, const int POS,
+                                        const int VALUE) {
+  if (POS < 0) {
+    return pHead;
+  }
+  Node* currentNode = pHead;
+  for (int i = 0; i < POS; i++) {
+    currentNode = currentNode->pNext;
+    if (currentNode == nullptr) {
+      return pHead;
+    }
+  }
+  currentNode->value = VALUE;
+  return pHead;
 }
